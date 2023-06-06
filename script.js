43d6f853824e3a198414c2b3846fc5a05e6c170b
Sumit <sumitdubey9974@gmail.com>
2023-06-06 15:29:08 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const bar = document.getElementById("bar");
+const nav = document.getElementById("navbar");
+const navClose = document.getElementById("close");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
+
