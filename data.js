24076ab6bbf30f0ce222a29fc9ba65df4a68cd46
Sumit9974 <sumitdubey9974@gmail.com>
2023-06-15 23:55:28 +0530

    Modified SingleProduct Details Display

--- data.js
+++ data.js
@@ -1,117 +1,88 @@
-const productContainer = document.getElementById("pro-container");
-const addProductButton = document.getElementById("add-product-button");
-const addModal = document.getElementById("add-modal");
-const modalContent = document.getElementsByClassName("modal__content");
-const backdrop = document.getElementById("backdrop");
-
-const renderProductButton = document.getElementById("modal-add-btn");
-const cancelButton = document.getElementById("modal-cancel-btn");
-
-addProductButton.addEventListener("click", showAddMovieBanner);
-renderProductButton.addEventListener("click", addProductToPage);
-cancelButton.addEventListener("click", removeAddMovieBanner);
-
 const productDetails = [
   {
-    id: 1,
-    name: "Cartoon Astronaut T-Shirts",
+    id: "a1cde",
+    name: "T-Shirts 1",
     brand: "adidas",
     price: 78,
     img: "img/products/f1.jpg",
   },
   {
-    id: 2,
-    name: "Cartoon Astronaut d-Shirts",
+    id: "s5fe5",
+    name: "T-Shirts 2",
     brand: "sssss",
-    price: 178,
+    price: 158,
     img: "img/products/f2.jpg",
   },
   {
-    id: 3,
-    name: "Cartoon Astronaut d-Shirts",
-    brand: "sssss",
-    price: 178,
+    id: "s58rd",
+    name: "T-Shirts 3",
+    brand: "Nike",
+    price: 190,
     img: "img/products/f3.jpg",
   },
   {
-    id: 4,
-    name: "Cartoon Astronaut d-Shirts",
-    brand: "sssss",
-    price: 178,
+    id: "gdne6",
+    name: "T-Shirts 4",
+    brand: "Adidas",
+    price: 224,
     img: "img/products/f4.jpg",
   },
   {
-    id: 5,
-    name: "Cartoon Astronaut d-Shirts",
-    brand: "sssss",
-    price: 178,
+    id: "6she5",
+    name: "T-Shirts 5",
+    brand: "Nike",
+    price: 326,
     img: "img/products/f5.jpg",
+  },
+  {
+    id: "dj8dy",
+    name: "T-Shirts 6",
+    brand: "Adidas",
+    price: 910,
+    img: "img/products/f6.jpg",
+  },
+  {
+    id: "eeee1",
+    name: "T-Shirts 7",
+    brand: "Adidas",
+    price: 512,
+    img: "img/products/f7.jpg",
+  },
+  {
+    id: "llop4",
+    name: "T-Shirts 8",
+    brand: "Nike",
+    price: 111,
+    img: "img/products/f8.jpg",
+  },
+  {
+    id: "wpls5",
+    name: "T-Shirts 9",
+    brand: "Nike",
+    price: 199,
+    img: "img/products/n1.jpg",
+  },
+  {
+    id: "jdi90",
+    name: "T-Shirts 10",
+    brand: "Adidas",
+    price: 99,
+    img: "img/products/n2.jpg",
+  },
+  {
+    id: "alops",
+    name: "T-Shirts 11",
+    brand: "Nike",
+    price: 410,
+    img: "img/products/n3.jpg",
+  },
+  {
+    id: "mmxn7",
+    name: "T-Shirts 12",
+    brand: "Adidas",
+    price: 110,
+    img: "img/products/n4.jpg",
   },
 ];
 
-function addProductToPage() {
-  const title = document.getElementById("title").value;
-  const brand = document.getElementById("brand").value;
-  const imageSrc = document.getElementById("image-url").value;
-  const price = document.getElementById("price").value;
-
-  const product = {
-    id: Math.random(),
-    name: title,
-    brand: brand,
-    price: price,
-    img: "img/products/" + imageSrc + ".jpg",
-  };
-
-  productDetails.push(product);
-  removeAddMovieBanner();
-  renderProduct();
-  clearInputs();
-}
-
-const renderProduct = () => {
-  return (productContainer.innerHTML = productDetails
-    .map((x) => {
-      let { id, name, brand, price, img } = x;
-      return ` <div class="pro" >
-                <img src="${img}" class="'pro-img" alt="">
-                <div class="description">
-                    <span class="pro-brand">${brand}</span>
-                    <h5 class="pro-title">${name}</h5>
-                    <div class="star">
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                        <i class="fas fa-star"></i>
-                    </div>
-                    <h4 class="pro-price">$ ${price}</h4>
-                </div>
-                <a href="#">
-                    <i class="fal fa-shopping-cart cart"></i>
-                </a>
-            </div>`;
-    })
-    .join(""));
-};
-
-renderProduct();
-
-// ===============================================================
-function showAddMovieBanner() {
-  addModal.classList.add("visible");
-  backdrop.classList.add("visible");
-}
-
-function removeAddMovieBanner() {
-  addModal.classList.remove("visible");
-  backdrop.classList.remove("visible");
-  clearInputs();
-}
-
-function clearInputs() {
-  const inputs = document.querySelectorAll("input");
-  for (let i = 0; i < inputs.length; i++) {
-    inputs[0].value = "";
-  }
-}
+export { productDetails };
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+import { productDetails } from "./data.js";
 const bar = document.getElementById("bar");
 const nav = document.getElementById("navbar");
 const navClose = document.getElementById("close");
 
 if (bar) {
-  bar.addEventListener("click", () => { 
+  bar.addEventListener("click", () => {
     nav.classList.add("active");
   });
 }
@@ -11,5 +12,98 @@ if (bar) {
 if (navClose) {
   navClose.addEventListener("click", () => {
     nav.classList.remove("active");
+  });
+}
+
+const productContainer = document.getElementById("pro-container");
+const addProductButton = document.getElementById("add-product-button");
+const addModal = document.getElementById("add-modal");
+const modalContent = document.getElementsByClassName("modal__content");
+const backdrop = document.getElementById("backdrop");
+const renderProductButton = document.getElementById("modal-add-btn");
+const cancelButton = document.getElementById("modal-cancel-btn");
+/*
+!===================================================================================================================
+*====================================================Shop Page Below================================================
+!===================================================================================================================
+*/
+
+function addProductToPage() {
+  const title = document.getElementById("title").value;
+  const brand = document.getElementById("brand").value;
+  const imageSrc = document.getElementById("image-url").value;
+  const price = document.getElementById("price").value;
+
+  const product = {
+    id: Math.random(),
+    name: title,
+    brand: brand,
+    price: price,
+    img: "img/products/" + imageSrc + ".jpg",
+  };
+
+  productDetails.push(product);
+  removeAddMovieBanner();
+  renderProduct();
+  clearInputs();
+}
+
+const renderProduct = () => {
+  return (productContainer.innerHTML = productDetails
+    .map((x) => {
+      let { id, name, brand, price, img } = x;
+      return ` <div class="pro" id="${id}" data-product-id>
+      <img src="${img}" class="pro-img" alt="">
+      <div class="description">
+      <span class="pro-brand">${brand}</span>
+      <h5 class="pro-title">${name}</h5>
+                <div class="star">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i> 
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                </div>
+                <h4 class="pro-price">$ ${price}</h4>
+                </div>
+                <div id="${id}" onclick="addToCart(${id})">
+                <a href="#">
+                <i class="fal fa-shopping-cart cart" ></i>
+                </a>
+                </div>
+                </div>`;
+    })
+    .join(""));
+};
+
+renderProduct();
+
+// ===============================================================
+function showAddMovieBanner() {
+  addModal.classList.add("visible");
+  backdrop.classList.add("visible");
+}
+
+function removeAddMovieBanner() {
+  addModal.classList.remove("visible");
+  backdrop.classList.remove("visible");
+  clearInputs();
+}
+addProductButton.addEventListener("click", showAddMovieBanner);
+renderProductButton.addEventListener("click", addProductToPage);
+cancelButton.addEventListener("click", removeAddMovieBanner);
+
+function clearInputs() {
+  const inputs = document.querySelectorAll("input");
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[0].value = "";
+  }
+}
+
+const product = productContainer.querySelectorAll(".pro");
+for (let i = 0; i < product.length; i++) {
+  product[i].addEventListener("click", (event) => {
+    const selectedItem = event.currentTarget.getAttribute("id");
+    window.location.href = "sproduct.html?id=" + selectedItem;
   });
 }
